9be05c0f72c076708fb8bbdffd5f6e64bf0c65a0
riky58 <rikytan@gmail.com>
2017-01-12 12:02:15 +1030

    First commit

---
+++ location-task/location-task/src/test/java/com/rikytan/locationtask/LocationTaskApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rikytan.locationtask;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LocationTaskApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
